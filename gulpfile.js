428fafc664f1869276be947d3d729de8a16ef066
taka1127 <taka07251127@gmail.com>
2020-05-25 00:22:01 +0900

    2020/5/24 intial

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp        = require('gulp');
+var watch       = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    watch('./**/*.html', function () {
+        browserSync.reload();
+    });
+});
+
+gulp.task('default', ['browser-sync','watch']);
